40d34eec5f04ebccc423f138cf73261e2c4f508b
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2025-05-18 15:05:26 +0300

    add: recover to gateway-service

--- services/gateway-service/infra/server/middleware.go
+++ services/gateway-service/infra/server/middleware.go
@@ -3,12 +3,15 @@ package server
 import (
 	"github.com/gofiber/contrib/otelfiber"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/kmlcnclk/kc-oms/services/gateway-service/infra/server/middlewares"
 	"github.com/sony/gobreaker"
 )
 
 func Middleware(app *fiber.App, cb *gobreaker.CircuitBreaker) {
 	app.Use(otelfiber.Middleware())
+
+	app.Use(recover.New())
 
 	app.Use(middlewares.CircuitBreakerMiddleware(cb, []string{}))
 
